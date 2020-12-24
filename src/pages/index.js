87d9c7679ab6d7fd5e713f144ca0b50e634ec2db
dcalsky <dcalsky@163.com>
2020-12-24 11:41:40 +0800

    feat: navigate to input page from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./index.less"
 import rabbitSVG from "../images/rabbit-index.svg"
 import heartSVG from "../images/heart.svg"
+import { navigate } from "gatsby"
 
 
 const IndexPage = () => (
@@ -10,7 +11,13 @@ const IndexPage = () => (
     <div className="bg">
       <img className="rabbit" src={rabbitSVG} alt="这里有个兔子" />
       <img className="heart" src={heartSVG} alt="这里有一些爱心" />
-      <button unselectable={true} className="generate">*开始生成我的2021*</button>
+      <button unselectable={true} className="generate" onClick={() => {
+        navigate(
+          "/input/",
+          {}
+        )
+      }}>*开始生成我的2021*
+      </button>
     </div>
   </>
 )
