eb004e9d1248d86e70bfad9c1a833bc493ec44fc
dcalsky <dcalsky@163.com>
2020-12-26 14:51:10 +0800

    feat: qr code and generating logic

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,40 +1,28 @@
-import React, { useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import "./result.less"
 import { navigate } from "gatsby"
 import { Stage, Layer, Rect, Text, Image } from "react-konva"
 import FontFaceObserver from "fontfaceobserver"
 import resultSVG from "../images/result.png"
 import useImage from "use-image"
+import qrImage from "../images/qr.png"
 
 const gap = 50
 const screenWidth = 375
 
-// const ResultImg = ({ imgWidth, imgHeight }) => {
-//   const [image] = useImage(resultSVG)
-//   if (!image) {
-//     return null
-//   } else {
-//     imgWidth = screenWidth - gap
-//     imgHeight = image.height / image.width * imgWidth
-//     return <Image x={gap / 2} y={gap / 2} width={imgWidth} height={imgHeight} image={image} />
-//   }
-// }
-
 const words = [
   "平安喜乐",
   "活泼圆润",
-  "简单快乐",
+  "简简单单",
   "奶茶自由",
   "发型酷炫",
   "胃口大开",
-  "好吃不胖",
-  "发量上涨",
+  "多吃不胖",
   "食欲大涨",
   "心态平和",
   "一身肌肉",
-  "身高增长",
-  "皮肤光滑",
-  "黝黑发亮",
+  "暖暖和和",
+  "吃饱喝足",
   "热水多喝",
   "生机勃勃",
   "健康养生",
@@ -44,7 +32,13 @@ const words = [
   "天天向上",
   "红红火火",
   "会穿秋裤",
-  "五颜六色"
+  "五颜六色",
+  "万物生长",
+  "就这样吧",
+  "重在参与",
+  "好吃好喝",
+  "充实日常",
+  "毫不离谱"
 ]
 
 const pickNWords = (n) => {
@@ -61,46 +55,58 @@ const pickNWords = (n) => {
   return arr
 }
 
-const downloadURI = (uri, name) => {
-  let link = document.createElement("a")
-  link.download = name
-  link.href = uri
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
-}
-
 const ResultPage = ({ location }) => {
+  let drawed = false
   const font = new FontFaceObserver("happy")
   const [loading, setLoading] = useState(true)
+  const [fontLoaded, setFontLoaded] = useState(false)
   location.state = location.state || {
     username: localStorage.getItem("username")
   }
+  const words = pickNWords(4)
   const { username } = location.state
-  const stageRef = React.useRef(null)
   const [image] = useImage(resultSVG)
-  let imgComponent = null
+  const [qrCode] = useImage(qrImage)
+  const [finalImage, setFinalImage] = useState(null)
+  const [stage, setStage] = useState(null)
   let imgWidth = 0
   let imgHeight = 0
   if (image) {
     imgWidth = screenWidth - gap
     imgHeight = image.height / image.width * imgWidth
-    imgComponent = <Image x={gap / 2} y={gap / 2} width={imgWidth} height={imgHeight} image={image} />
   }
-  const words = pickNWords(4)
   font.load().then(() => {
-    setLoading(false)
+    setFontLoaded(true)
   })
+  useEffect(() => {
+    if (image && fontLoaded) {
+      setLoading(false)
+    }
+  }, [image, fontLoaded])
+  const measuredRef = useCallback(node => {
+    if (node !== null && !loading) {
+      setStage(node)
+    }
+  }, [loading])
+
+  useEffect(() => {
+    if (stage) {
+      stage.toImage({
+        callback(img) {
+          setFinalImage(img)
+          drawed = true
+        },
+        mimeType: "image/jpeg",
+        quality: 1,
+        pixelRatio: 2
+      })
+    }
+  }, [stage])
   const goback = () => {
-    navigate("/input")
+    navigate("/input", { replace: true })
   }
   const saveImage = () => {
-    const uri = stageRef.current.toDataURL({
-      mimeType: "image/jpeg",
-      quality: 1,
-      pixelRatio: 2
-    })
-    downloadURI(uri, "rabbit-2021.png")
+    alert("请长按图片保存")
   }
   return (
     <div className="result-page">
@@ -110,12 +116,12 @@ const ResultPage = ({ location }) => {
         </div>
         {
           loading ? <h1>生成中...</h1> :
-            <Stage ref={stageRef} className="stage" width={screenWidth} height={640}>
+            <Stage ref={measuredRef} className="stage" width={screenWidth} height={640}>
               <Layer>
                 <Rect width={screenWidth} height={640} fill="#ffffff" />
               </Layer>
               <Layer>
-                {imgComponent}
+                <Image x={gap / 2} y={gap / 2} width={imgWidth} height={imgHeight} image={image} />
                 <Text fontFamily="happy" x={175} y={115} fontSize={23} width={48} height={48} fill="#ffffff"
                       text={words[0]} />
                 <Text fontFamily="happy" x={218} y={175} fontSize={23} width={48} height={48} fill="#ffffff"
@@ -130,9 +136,15 @@ const ResultPage = ({ location }) => {
                       text={"寒来暑往，新年快乐。"} />
                 <Text x={gap / 2} y={imgHeight + 45 + 40 + 25} fontSize={12} fontStyle="bold" fill="#C7C7C7"
                       text={"长按图片 一起生成你的「2021」"} />
+                <Image image={qrCode} width={64} height={64} x={screenWidth - gap / 2 - 64}
+                       y={imgHeight + 45} />
               </Layer>
             </Stage>
         }
+        {
+          finalImage ? <img className="result-image" src={finalImage.src} alt="" /> : null
+        }
+
         <div className="tools">
           <button className="generate play-again" onClick={goback}>再来一次
           </button>
