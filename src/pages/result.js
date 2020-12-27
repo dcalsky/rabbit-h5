d660b4b154f20e3739594d9c1ef592e5365199af
dcalsky <dcalsky@163.com>
2020-12-27 14:11:46 +0800

    fix: index page image width

--- src/pages/result.js
+++ src/pages/result.js
@@ -6,7 +6,7 @@ import FontFaceObserver from "fontfaceobserver"
 import useImage from "use-image"
 import { loadingImg, qrImg, resultImg } from "../constants"
 
-const gap = 50
+const gap = 40
 const screenWidth = 375
 
 const words = [
@@ -120,12 +120,12 @@ const ResultPage = ({ location }) => {
               style={{ marginTop: 30 }}
               src={loadingImg} alt="Loading" />
             :
-            <Stage ref={measuredRef} className="stage" width={screenWidth} height={640}>
+            <Stage ref={measuredRef} className="stage" width={screenWidth} height={642}>
               <Layer>
-                <Rect width={screenWidth} height={640} fill="#ffffff" />
+                <Rect width={screenWidth} height={642} fill="#ffffff" />
               </Layer>
               <Layer>
-                <Image x={gap / 2} y={gap / 2} width={imgWidth} height={imgHeight} image={image} />
+                <Image x={gap / 2} y={18} width={imgWidth} height={imgHeight} image={image} />
                 <Text fontFamily="happy" x={170} y={115} fontSize={23} width={48} height={48} fill="#ffffff"
                       text={words[0]} />
                 <Text fontFamily="happy" x={215} y={175} fontSize={21} width={48} height={48} fill="#ffffff"
@@ -134,12 +134,12 @@ const ResultPage = ({ location }) => {
                       text={words[2]} />
                 <Text fontFamily="happy" x={108} y={205} fontSize={21} width={48} height={48} fill="#ffffff"
                       text={words[3]} />
-                <Text width={270} x={gap / 2} height={10} y={imgHeight + 45} fontSize={23} fontStyle="bold"
+                <Text width={270} x={gap / 2} height={10} y={imgHeight + 40} fontSize={23} fontStyle="bold"
                       fill="#A1A1A1"
                       text={`2021的${username}`} />
-                <Text x={gap / 2} y={imgHeight + 42 + 40} fontSize={18} fontStyle="bold" fill="#A1A1A1"
+                <Text x={gap / 2} y={imgHeight + 40 + 38} fontSize={18} fontStyle="bold" fill="#A1A1A1"
                       text={"寒来暑往，新年快乐。"} />
-                <Text x={gap / 2} y={imgHeight + 42 + 40 + 25} fontSize={12} fontStyle="bold" fill="#C7C7C7"
+                <Text x={gap / 2} y={imgHeight + 40 + 38 + 25} fontSize={12} fontStyle="bold" fill="#C7C7C7"
                       text={"长按图片 一起生成你的「2021」"} />
                 <Image image={qrCode} width={64} height={64} x={screenWidth - gap / 2 - 64}
                        y={imgHeight + 45} />
